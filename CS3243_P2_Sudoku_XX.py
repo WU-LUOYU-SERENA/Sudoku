19ac97d6d2ef2a1fcca16286fb465111b1a7d723
WU_LUOYU_SERENA <siriuswusss@gmail.com>
2021-10-09 01:06:08 +0800

    Try Priority Queue for choosing the most constrained unassigned variable
    Taking much longer for some reason

--- CS3243_P2_Sudoku_XX.py
+++ CS3243_P2_Sudoku_XX.py
@@ -17,27 +17,32 @@ class Sudoku(object):
         unassigned = self.computeUnAssignedVariables(self.ans)
         self.unassignedVars = self.preprocessVar(unassigned)
 
+    def computeUnAssignedVariables(self, currAns):
+        unassigned = PriorityQueue()
+        for i in range(9):
+            for j in range(9):
+                if currAns[i][j] == 0:
+                    unassigned.push(((i, j), range(1, 10)))
+        return unassigned
+
     def preprocessVar(self, unassigned):
         unassignedVars = copy.deepcopy(unassigned)
+        len = unassigned.count
         assert id(unassignedVars) != id(unassigned)
-        length = len(unassigned)
-        iterator = 0
-        while iterator < length:
-            (x, y), domain = copy.deepcopy(unassigned[iterator])
-            index = unassignedVars.index(((x, y), domain))
+        for index, (priority, c, ((x, y), domain)) in enumerate(unassignedVars.heap):
+            # unassigned[index] = priority, count, item
             positions = self.findConstraintPositions((x, y))
             for (i, j) in positions:
                 val = self.puzzle[i][j]
                 if val != 0:
-                    if val in unassignedVars[index][1]:
-                        unassignedVars[index][1].remove(val)
-                if len(unassignedVars[index][1]) == 1:
-                    self.ans[x][y] = unassignedVars[index][1][0]
-                    unassignedVars.pop(index)
-                    break
-            iterator += 1
-        newLen = len(unassigned)
-        assert newLen == length
+                    if val in domain:
+                        domain.remove(val)
+                        len = unassignedVars.update(((x, y), domain))
+                        if len == 1:
+                            self.ans[x][y] = domain[0]
+                            assert self.isConsistent(self.ans)
+                            unassignedVars.remove(((x, y), domain))
+                            break
         return unassignedVars
 
     def allVarsAreAssigned(self, currAns):
@@ -85,6 +90,9 @@ class Sudoku(object):
                     return False
         return True
 
+    def solve(self):
+        return self.backtrackingWithInfer(self.ans, self.unassignedVars)
+
     def backtrackingWithInfer(self, currAns, currUnassignedVars):
         if self.allVarsAreAssigned(currAns):
             return currAns
@@ -102,18 +110,49 @@ class Sudoku(object):
             currAns[x][y] = 0
         return False
 
-
-    def solve(self):
-        return self.backtrackingWithInfer(self.ans, self.unassignedVars)
-        #return self.puzzle
-
-    def computeUnAssignedVariables(self, currAns):
-        unassignedVars = list()
-        for i in range(9):
-            for j in range(9):
-                if currAns[i][j] == 0:
-                    unassignedVars.insert(0, ((i, j), range(1, 10)))
-        return unassignedVars
+    def infer(self, (i, j), currAns, currUnassignedVars):
+        # inference: list of tuples in the form of
+        #   ((x-coordinate, y-coordinate), [values the variable cannot take])
+        nextAns = copy.deepcopy(currAns)
+        nextUnassignedVars = copy.deepcopy(currUnassignedVars)
+        varQueue = Queue()
+        # varQueue.push((i, j))
+        # do part of do-while loop
+        # (x, y) = varQueue.pop()
+        constrainedVars = self.findConstraintPositions((i, j))
+        val = nextAns[i][j]
+        for index, (priority, c, ((xx, yy), domain)) in enumerate(nextUnassignedVars.heap):
+            assert priority == len(domain)
+            if not (xx, yy) in constrainedVars:
+                continue
+            if val in domain:
+                domain.remove(val)
+                nextUnassignedVars.update(((xx, yy), domain))
+                if len(domain) == 0:
+                    return False
+                if len(domain) == 1:
+                    varQueue.push(((xx, yy), domain))
+        # while part of  do-while loop
+        while not varQueue.isEmpty():
+            (x, y), domainOfLenOne = varQueue.pop()
+            assert len(domainOfLenOne) == 1
+            nextAns[x][y] = domainOfLenOne[0]
+            if self.isConsistent(nextAns):
+                nextUnassignedVars.remove(((x, y), domainOfLenOne))
+                constrainedPos = self.findConstraintPositions((x, y))
+                for index, (priority, c, ((xx, yy), domainOfXxYy)) in enumerate(nextUnassignedVars.heap):
+                    if not (xx, yy) in constrainedPos:
+                        continue
+                    if nextAns[x][y] in domainOfXxYy:
+                        domainOfXxYy.remove(nextAns[x][y])
+                        nextUnassignedVars.update(((xx, yy), domainOfXxYy))
+                        if len(domainOfXxYy) == 0:
+                            return False
+                        if len(domainOfXxYy) == 1:
+                            varQueue.push(((xx, yy), domainOfXxYy))
+            else:
+                return False
+        return nextAns, nextUnassignedVars
 
     def findConstraintPositions(self, (x, y)):
         positions = set()
@@ -141,46 +180,6 @@ class Sudoku(object):
         if xx == 2:
             return range (x-2, x+1)
 
-    def infer(self, (i, j), currAns, currUnassignedVars):
-        # inference: list of tuples in the form of
-        #   ((x-coordinate, y-coordinate), [values the variable cannot take])
-        nextAns = copy.deepcopy(currAns)
-        nextUnassignedVars = copy.deepcopy(currUnassignedVars)
-        varQueue = Queue()
-        #varQueue.push((i, j))
-        # do part of do-while loop
-        #(x, y) = varQueue.pop()
-        constrainedVars = self.findConstraintPositions((i, j))
-        for (xx, yy), domain in nextUnassignedVars:
-            if not (xx, yy) in constrainedVars:
-                continue
-            if nextAns[i][j] in domain:
-                domain.remove(nextAns[i][j])
-                if len(domain) == 0:
-                    return False
-                if len(domain) == 1:
-                    varQueue.push(((xx, yy), domain))
-        # while part of  do-while loop
-        while not varQueue.isEmpty():
-            (x, y), domainOfLenOne = varQueue.pop()
-            assert len(domainOfLenOne) == 1
-            nextAns[x][y] = domainOfLenOne[0]
-            if self.isConsistent(nextAns):
-                nextUnassignedVars.remove(((x, y), domainOfLenOne))
-                constrainedPos = self.findConstraintPositions((x, y))
-                for (xx, yy), domainOfXY in nextUnassignedVars:
-                    if not (xx, yy) in constrainedPos:
-                        continue
-                    if nextAns[x][y] in domainOfXY:
-                        domainOfXY.remove(nextAns[x][y])
-                        if len(domainOfXY) == 0:
-                            return False
-                        if len(domainOfXY) == 1:
-                            varQueue.push(((xx, yy), domainOfXY))
-            else:
-                return False
-        return nextAns, nextUnassignedVars
-
     # you may add more classes/functions if you think is useful
     # However, ensure all the classes/functions are in this file ONLY
     # Note that our evaluation scripts only call the solve method.
@@ -197,7 +196,8 @@ class PriorityQueue:
         self.heap = []
         self.count = 0
 
-    def push(self, item, priority):
+    def push(self, item): # item = (x,y), domain
+        priority = len(item[1]) # priority is the size of domain
         entry = (priority, self.count, item)
         heapq.heappush(self.heap, entry)
         self.count += 1
@@ -209,20 +209,37 @@ class PriorityQueue:
     def isEmpty(self):
         return len(self.heap) == 0
 
-    def update(self, item, priority):
-        # If item already in priority queue with higher priority, update its priority and rebuild the heap.
-        # If item already in priority queue with equal or lower priority, do nothing.
-        # If item not in priority queue, do the same thing as self.push.
+    def remove(self, item):
+        for index, (p, c, i) in enumerate(self.heap):
+            if i == item: # find the item in the heap
+                del self.heap[index]
+                self.count -= 1
+                heapq.heapify(self.heap)
+                return
+        return False
+
+    def index(self, item):
         for index, (p, c, i) in enumerate(self.heap):
             if i == item:
+                return index
+        return False
+
+    def update(self, (coordinates, domain)): # item = (x, y), domain
+        # If item already in priority queue with higher priority, update its priority and rebuild the heap.
+        # If item already in priority queue with equal or lower priority, return false.
+        # If item not in priority queue, return false.
+        priority = len(domain) # priority is the size of domain
+        for index, (p, c, i) in enumerate(self.heap):
+            if i[0] == coordinates:
                 if p <= priority:
-                    break
+                    return False
                 del self.heap[index]
-                self.heap.append((priority, c, item))
+                self.heap.append((priority, c, (coordinates, domain)))
                 heapq.heapify(self.heap)
+                return priority
                 break
-        else:
-            self.push(item, priority)
+        return False
+
 
 class PriorityQueueWithFunction(PriorityQueue):
     """
@@ -262,13 +279,14 @@ class Queue:
         return len(self.list) == 0
 
 if __name__ == "__main__":
+    '''
     # STRICTLY do NOT modify the code in the main function here
     if len(sys.argv) != 3:
         print ("\nUsage: python CS3243_P2_Sudoku_XX.py input.txt output.txt\n")
         raise ValueError("Wrong number of arguments!")
-
+    '''
     try:
-        f = open(sys.argv[1], 'r')
+        f = open('CS3243_P2_Sudoku_XX/input_3.txt', 'r')
     except IOError:
         print ("\nUsage: python CS3243_P2_Sudoku_XX.py input.txt output.txt\n")
         raise IOError("Input file not found!")
@@ -289,7 +307,7 @@ if __name__ == "__main__":
     sudoku = Sudoku(puzzle)
     ans = sudoku.solve()
 
-    with open(sys.argv[2], 'a') as f:
+    with open('CS3243_P2_Sudoku_XX/output_3.txt', 'a') as f:
         for i in range(9):
             for j in range(9):
                 f.write(str(ans[i][j]) + " ")
